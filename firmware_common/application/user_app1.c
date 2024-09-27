4636de26d34c3c1e3557866f6bb5b5e89cc6e687
MrEpicx2 <xavierb2@outlook.com>
2024-09-26 19:25:07 -0600

    Update user_app1.c

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -2,7 +2,7 @@
 @file user_app1.c                                                                
 @brief User's tasks / applications are written here.  This description
 should be replaced by something specific to the task.
-
+#Ballin
 ----------------------------------------------------------------------------------------------------------------------
 To start a new task using this user_app1 as a template:
  1. Copy both user_app1.c and user_app1.h to the Application directory
