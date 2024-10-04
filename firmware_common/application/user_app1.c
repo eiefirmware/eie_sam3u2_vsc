4a2b99f5f6ccf11eece327bc998da5695cdc8344
Kris <78143563+kr1s-r@users.noreply.github.com>
2024-10-03 19:24:46 -0600

    testing commit

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -3,6 +3,8 @@
 @brief User's tasks / applications are written here.  This description
 should be replaced by something specific to the task.
 
+TESTING ON NEW BRANCH "testing"
+
 ----------------------------------------------------------------------------------------------------------------------
 To start a new task using this user_app1 as a template:
  1. Copy both user_app1.c and user_app1.h to the Application directory
