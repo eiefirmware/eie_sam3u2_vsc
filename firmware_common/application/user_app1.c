8f86401df3e61920ec2b11cf2fa99d9fc9d3d5bf
yusun23 <160289921+yusun23@users.noreply.github.com>
2024-10-18 02:50:40 -0600

    Led Basics
    Finished version of Led Basics

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -92,11 +92,34 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
+  /*
   // Basic Functions of the Matrix showing different capabilities of different functions.
+  // Turn on the left side blue LED with the LedOn function
   LedOn(BLUE0);
+  // Turn on the right side red LED with the LedToggle function
   LedToggle(RED3);
+  // Set the 3rd LED to blink green
   LedBlink(GREEN2, LED_2HZ);
+  // Set the 2nd LED to the dimmest level available
   LedPWM(BLUE1, LED_PWM_5);
+  */
+  LedOff(RED0);
+  LedOff(GREEN0);
+  LedOff(BLUE0);
+
+  LedOff(RED1);
+  LedOff(GREEN1);
+  LedOff(BLUE1);
+
+  LedOff(RED2);
+  LedOff(GREEN2);
+  LedOff(BLUE2);
+
+  LedOff(RED3);
+  LedOff(GREEN3);
+  LedOff(BLUE3);
+
+  LedOff(LCD_BL);
   /* If good initialization, set state to Idle */
   if( 1 )
   {
@@ -146,12 +169,108 @@ State Machine Function Definitions
 static void UserApp1SM_Idle(void)
 {
      static u16 u16BlinkCount = 0;
+     static u8 u8Counter = 0;
+     static u8 u8ColorIndex = 0;
+     // Colors: Red, Yellow, Green, Cyan, Blue, Purple, White
+     static u8 aau8Color[7][3] = {{RED0, 0xff, 0xff}, 
+                                 {RED0, GREEN0, 0xff}, 
+                                 {0xff, GREEN0, 0xff},
+                                 {0xff, GREEN0, BLUE0},
+                                 {0xff, 0xff, BLUE0},
+                                 {RED0, 0xff, BLUE0},
+                                 {RED0, GREEN0, BLUE0},};
+     u8 u8Offset = 0;
+     // Using knowledge of LedNameType to use a loop for initializing all LEDs to off state
+     for(u8 i = 0; i < (U8_TOTAL_LEDS - 1); i++)
+     {
+      LedOff((LedNameType)i);
+     }
+     // Set color for each bit (LED) in the 4-bit counter
+    if(u8Counter & 0x01)
+    {
+      u8Offset = 3; // The numerical difference between LED elements of the same color (e.g. RED3 - RED0 = 3)
+       for(u8 j = 0; j < 3; j++)
+       {
+        if(aau8Color[u8ColorIndex][j] != 0xff)
+        {
+          LedOn((aau8Color[u8ColorIndex][j]) + u8Offset);
+        }
+      }
+    }
+    if(u8Counter & 0x02)
+    {
+      u8Offset = 2; // The numerical difference between LED elements of the same color (e.g. RED3 - RED0 = 3)
+      for(u8 j = 0; j < 3; j++)
+      {
+        if(aau8Color[u8ColorIndex][j] != 0xff)
+        {
+           LedOn((aau8Color[u8ColorIndex][j]) + u8Offset);
+        }
+      }
+    }
+    if(u8Counter & 0x04)
+    {
+      u8Offset = 1; // The numerical difference between LED elements of the same color (e.g. RED3 - RED0 = 3)
+      for(u8 j = 0; j < 3; j++)
+      {
+        if(aau8Color[u8ColorIndex][j] != 0xff)
+        {
+          LedOn((aau8Color[u8ColorIndex][j]) + u8Offset);
+        }
+       }
+     }
+    if(u8Counter & 0x08)
+    {
+      u8Offset = 0; // The numerical difference between LED elements of the same color (e.g. RED3 - RED0 = 3)
+      for(u8 j = 0; j < 3; j++)
+      {
+        if(aau8Color[u8ColorIndex][j] != 0xff)
+        {
+          LedOn((aau8Color[u8ColorIndex][j]) + u8Offset);
+        }
+      }
+    } 
      u16BlinkCount++;
      if (u16BlinkCount == 250)
      {
-        LedToggle(RED3);
+        //LedToggle(RED3);
         u16BlinkCount = 0;
+        u8Counter++;
+        if(u8Counter == 16)
+        {
+          u8Counter = 0;
+          u8ColorIndex++;
+          if (u8ColorIndex > 7)
+          {
+            u8ColorIndex = 0;
+          }
+        }
      }
+     /*
+     LedOff(RED3);
+     LedOff(GREEN2);
+     LedOff(BLUE1);
+     LedOff(BLUE0);
+     LedOff(RED0);
+
+     if(u8Counter & 1)
+     {
+      LedOn(RED3);
+     }
+     if(u8Counter & 2)
+     {
+      LedOn(GREEN2);
+     }
+     if(u8Counter & 4)
+     {
+      LedOn(BLUE1);
+     }
+     if(u8Counter & 8)
+     {
+      LedOn(RED0);
+      LedOn(BLUE0);
+     }
+    */
 } /* end UserApp1SM_Idle() */
      
 
