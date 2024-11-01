32e4d080e141e643e1636813f4382fde11302d9e
danielje <150391560+danielje-obtuse@users.noreply.github.com>
2024-10-31 18:42:55 -0600

    Update user_app1.c

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -155,9 +155,6 @@ static void UserApp1SM_Idle(void)
 {
  static u16 u16_heartbeat_counter = U16_COUNTER_PERIOD_MS;
  static int turn_light = 0;
- static u8 u8_password[10] = {0, 0, 1, 2}, u8_inputted_password[10];
- static u8 u8_passwordindex = 0;
- bool bool_correct_password = TRUE;
 
  u16_heartbeat_counter -= 1;
  if (u16_heartbeat_counter == 0) { 
