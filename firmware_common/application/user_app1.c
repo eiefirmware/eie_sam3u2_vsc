2bf363f0ff93c2ccc38184dc5e0ed1fc0fe7afc7
danielje <150391560+danielje-obtuse@users.noreply.github.com>
2024-11-17 18:38:18 -0700

    Update user_app1.c

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -161,7 +161,7 @@ static void UserApp1SM_Idle(void)
  }
 
  static u8 u8NumCharsMessage[] = "\n\rCharacters in buffer: ";
- static u8 u8BufferMessage[] = "\n\rBuffer contents:";
+ static u8 u8BufferMessage[] = "\n\rBuffer contents:\n";
  u8 u8CharCount;
  if (WasButtonPressed(BUTTON0)){
   ButtonAcknowledge(BUTTON0);
